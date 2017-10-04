4fbe032a58bdda63122d02089cac77b6a736fbd3
Maciej Sawicki <sawik11@wp.pl>
2017-10-04 20:51:23 +0200

    Initial commit

---
+++ api/src/test/kotlin/pl/bas/microtwitter/MicrotwitterApplicationTests.kt
@@ -0,0 +1,16 @@
+package pl.bas.microtwitter
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class MicrotwitterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
