64c70ac6daee940504349e41c7e13fcb078d52d9
Maciej Sawicki <sawik11@wp.pl>
2017-11-18 00:35:52 +0100

    Added main router.
    Added global rxjs import

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import 'rxjs';
+import 'rxjs';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
